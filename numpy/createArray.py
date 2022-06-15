2357229cadfa6fa91b19a8d8ec45186711ca750d
wjbulikescoding <voquangvu09112006@gmail.com>
2022-06-15 16:58:06 +0700

    upload files

---
+++ numpy/createArray.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+arr = np.array([1, 2, 3, 4, 5])
+
+print(arr)
+
+print(np.__version__)
+
+print(type(arr))
+#numpy.ndarray
+arr = np.array((1, 2, 3, 4, 5))
+
+print(arr)
+
+print(type(arr))
+
+a = np.array(42)
+b = np.array([1, 2, 3, 4, 5])
+c = np.array([[1, 2, 3], [4, 5, 6]])
+d = np.array([[[1, 2, 3], [4, 5, 6]], [[1, 2, 3], [4, 5, 6]]])
+
+print(a)
+print(b)
+print(c)
+print(d)
+#print dimension of array
+print(a.ndim)
+print(b.ndim)
+print(c.ndim)
+print(d.ndim)
