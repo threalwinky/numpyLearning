bd9f6a6c0ee8ed9c33df4093ed1726fad211c869
wjbulikescoding <voquangvu09112006@gmail.com>
2022-06-16 11:18:38 +0700

    cm

--- numpy/arraySlice.py
+++ numpy/arraySlice.py
@@ -1,7 +1,9 @@
 import numpy as np
 
-arr = np.array([1, 2, 3, 4, 5, 6, 7])
+arr = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9])
 #2 - > 5
 print(arr[1:5])
 #4 - > 6
 print(arr[3:-1])
+#3   5   7
+print(arr[2:8:2])
